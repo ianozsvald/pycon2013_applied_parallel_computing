a0f31d0b0ad843b70503489f48eae3029a98ba1e
pycon <pycon@pycon-VirtualBox.(none)>
2013-02-02 04:26:22 -0800

    .

--- setup.sh
+++ setup.sh
@@ -2,93 +2,126 @@
 
 tab="          ";
 
-sudo -n pwd > /dev/null 2>&1;
-if [ $? != 0 ]; then
-   echo "#";
-   echo "# Error: Seems like 'sudo' is setup to prompt for";
-   echo "#        password. Please augment /etc/sudoers by";
-   echo "#        including the following line";
-   echo "          ${HOME} ALL=(ALL) NOPASSWD: ALL";
-   echo "#";
-   exit 1;
-fi;
+# ---------------------------------------------------------------------------
 
-if [ ! -d "${DISCO_HOME}" ]; then
-   discoHome=`cd \`dirname $0\` && pwd`/usr/DISCO_HOME;
-   echo "#";
-   echo "# Error: 'DISCO_HOME' must be set to:";
-   echo "          ${discoHome}";
-   echo "#";
-   exit 1;
-fi;
+checkBasic ()
+{
+    # sudo must work without prompting for password!
+    sudo -n pwd > /dev/null 2>&1;
+    if [ $? != 0 ]; then
+	echo "#";
+	echo "# Error: Seems like 'sudo' is setup to prompt for";
+	echo "#        password. Please augment /etc/sudoers by";
+	echo "#        including the following line";
+	echo "          ${HOME} ALL=(ALL) NOPASSWD: ALL";
+	echo "#";
+	exit 1;
+    fi;
 
-echo "# Info: Installing required packages ...";
-(
-  for pkg in git                 \
-             openssh-server      \
-             expect              \
-                                 \
-             libblas-dev         \
-  	     liblapack-dev       \
-    	     gfortran            \
-                                 \
-  	     python-dev          \
-	     python-pip          \
-             python-dateutil     \
-             python-numpy        \
-             cython              \
-             python-scipy        \
-             python-sklearn      \
-                                 \
-             erlang-base         \
-             erlang              \
-             ; do                
-     echo "#${tab}${pkg} " ;
-     sudo apt-get install           \
-            -y ${pkg}               \
-            >/dev/null 2>&1;
+    # DISCO_HOME must point to './usr/DISCO_HOME'
+    discoHome=`cd \`dirname $0\` && pwd`/usr/DISCO_HOME;
+    if [ "${DISCO_HOME}" != ${discoHome} ]; then
+	echo "#";
+	echo "# Error: 'DISCO_HOME' must be set to:";
+	echo "          ${discoHome}";
+	echo "#";
+	exit 1;
+    fi;
+}
 
-     if [ $? != 0 ]; then 
-        exit 1;
-     fi;
-  done;                  
+# ---------------------------------------------------------------------------
 
-  for pkg in pillow \
-             pp     \
-             ; do 
-     (cd /dev/shm ; ls -1 | grep -i ${pkg} | xargs -n 1 sudo rm -rf) > /dev/null 2>&1;
-     echo "#${tab}${pkg}" ;
-     sudo pip install        \
-            -b /dev/shm      \
-               ${pkg}        \
-            >  /dev/null 2>&1;
-     stat=$?;
+pkgBasic()
+{
+    echo "# Info: Installing required packages ...";
+    (
+	for pkg in              \
+	    git                 \
+            openssh-server      \
+            expect              \
+            \
+            libblas-dev         \
+  	    liblapack-dev       \
+    	    gfortran            \
+            \
+  	    python-dev          \
+	    python-pip          \
+            python-dateutil     \
+            python-numpy        \
+            cython              \
+            python-scipy        \
+            python-sklearn      \
+            \
+            erlang-base         \
+            erlang              \
+            ; do                
+	    echo "#${tab}${pkg} " ;
+	    sudo apt-get install  \
+		-y ${pkg}         \
+		>/dev/null 2>&1;
+	    if [ $? != 0 ]; then 
+		exit 1;
+	    fi;
+	done;                  
 
-     (cd /dev/shm ; ls -1 | grep -i ${pkg} | xargs -n 1 sudo rm -rf) > /dev/null 2>&1;
-     if [ ${stat} != 0 ]; then 
-        exit 1;
-     fi;
-  done;
+	for pkg in \
+	    pillow \
+            pp     \
+            ; do 
+	    (cd /dev/shm ; ls -1 | grep -i ${pkg} | xargs -n 1 sudo rm -rf) > /dev/null 2>&1;
+	    echo "#${tab}${pkg}" ;
+	    sudo pip install     \
+		-b /dev/shm      \
+		${pkg}           \
+		>  /dev/null 2>&1;
+	    stat=$?;
 
-  exit 0;
-);
+	    (cd /dev/shm ; ls -1 | grep -i ${pkg} | xargs -n 1 sudo rm -rf) > /dev/null 2>&1;
+	    if [ ${stat} != 0 ]; then 
+		exit 1;
+	    fi;
+	done;
+	
+	exit 0;
+    );
 
-if [ $? != 0 ]; then
-   echo "#";
-   echo "# Error: Could not install packages. Am quitting.";
-   echo "#";
-   exit 1;
-fi;
+    if [ $? != 0 ]; then
+	echo "#";
+	echo "# Error: Could not install packages. Am quitting.";
+	echo "#";
+	exit 1;
+    fi;
+}
 
-./usr/DISCO_HOME.setup.sh;
+# ---------------------------------------------------------------------------
 
-echo "# Info: Compiling demos ...";
-(cd ./2_MapReduceDisco/word_count_cloud/word_cloud; make) > /dev/null 2>&1;
-if [ $? != 0 ]; then
-   echo "#";
-   echo "# Error: Could not compile demos. Am quitting.";
-   echo "#";
-   exit 1;
-fi;
+pkgDISCO ()
+{
+    ./usr/DISCO_HOME.setup.sh;
+}
 
-exit 0;
+# ---------------------------------------------------------------------------
+
+pkgDemo ()
+{
+    echo "# Info: Compiling demos ...";
+    (cd ./2_MapReduceDisco/word_count_cloud/word_cloud; make) > /dev/null 2>&1;
+    if [ $? != 0 ]; then
+	echo "#";
+	echo "# Error: Could not compile demos. Am quitting.";
+	echo "#";
+	exit 1;
+    fi;
+    
+    exit 0;
+}
+
+# ---------------------------------------------------------------------------
+
+checkBasic;
+pkgBasic  ;
+pkgDISCO  ;
+pkgDemo   ;
+
+# ---------------------------------------------------------------------------
+
