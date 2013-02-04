8c7683a28dd62b1fab572d06e62fb984c09dd0a4
Ian Ozsvald <ian@ianozsvald.com>
2013-02-04 22:05:50 +0000

    first

---
+++ 1_ListOfTasks/solutions/hotq.py
@@ -0,0 +1,162 @@
+"""HotQueue Redis example, use --server flag to setup work and consume without the --server flag"""
+import argparse
+import datetime
+import time
+import hotqueue
+import plotting
+
+# Usage:
+# $ python serial_python.py --server
+# which will setup a series of jobs in Redis via a HotQueue
+
+# area of space to investigate
+x1, x2, y1, y2 = -2.13, 0.77, -1.3, 1.3
+
+# plot in 2D for a fast output or 3D for a slower but prettier output
+SHOW_IN_3D = False
+
+# setup input and output queues
+hq_in = hotqueue.HotQueue('mandelbrot_in')
+hq_out = hotqueue.HotQueue('mandelbrot_out')
+
+
+def calculate_z((q, maxiter)):
+    """Pure python with complex datatype, iterating over list of q and z"""
+    output = [0] * len(q)
+    for i in range(len(q)):
+        zi = 0 + 0j
+        qi = q[i]
+        if i % 1000 == 0:
+            # print out some progress info since it is so slow...
+            print "%0.2f%% complete" % (1.0 / len(q) * i * 100)
+
+        output[i] = maxiter  # force max value if we exceed maxiter
+
+        for iteration in range(maxiter):
+            zi = zi * zi + qi
+            if abs(zi) > 2.0:
+                output[i] = iteration
+                break
+    return output
+
+
+def calc_pure_python(show_output, args):
+    # make a list of x and y values which will represent q
+    # xx and yy are the co-ordinates, for the default configuration they'll look like:
+    # if we have a 1000x1000 plot
+    # xx = [-2.13, -2.1242, -2.1184000000000003, ..., 0.7526000000000064, 0.7584000000000064, 0.7642000000000064]
+    # yy = [1.3, 1.2948, 1.2895999999999999, ..., -1.2844000000000058, -1.2896000000000059, -1.294800000000006]
+    x_step = (float(x2 - x1) / float(w)) * 2
+    y_step = (float(y1 - y2) / float(h)) * 2
+    x = []
+    y = []
+    ycoord = y2
+    while ycoord > y1:
+        y.append(ycoord)
+        ycoord += y_step
+    xcoord = x1
+    while xcoord < x2:
+        x.append(xcoord)
+        xcoord += x_step
+    q = []
+    for ycoord in y:
+        for xcoord in x:
+            q.append(complex(xcoord, ycoord))
+
+    print "Total elements:", len(q)
+
+    # split work list into continguous chunks, one per CPU
+    # build this into chunks which we'll apply to map_async
+    nbr_chunks = 10  # experiment with different nbrs of chunks
+    print "Redis HotQueue using {} chunks of data".format(nbr_chunks)
+    chunk_size = len(q) / nbr_chunks
+
+    # split our long work list into smaller chunks
+    # make sure we handle the edge case where nbr_chunks doesn't evenly fit into len(q)
+    if len(q) % nbr_chunks != 0:
+        # make sure we get the last few items of data when we have
+        # an odd size to chunks (e.g. len(q) == 100 and nbr_chunks == 3
+        nbr_chunks += 1
+    chunks = [(q[x * chunk_size:(x + 1) * chunk_size], maxiter) for x in xrange(nbr_chunks)]
+    print chunk_size, len(chunks), len(chunks[0][0])
+
+    start_time = datetime.datetime.now()
+
+    if args.server:
+        # reset queues
+        hq_in.clear()
+        hq_out.clear()
+
+        # post chunks to the hq_in HotQueue
+        for chunk_id, chunk in enumerate(chunks):
+            print "Submitting job with len(q) {}".format(len(chunk[0]))
+            hq_in.put((chunk_id, chunk))
+
+        # wait until the jobs are complete
+        while len(hq_out) != len(chunks):
+            if len(hq_out) > 0:
+                print "We can see {} computed chunks".format(len(hq_out))
+                time.sleep(0.1)
+
+        # the chunks may have been posted out of order
+        results = []
+        while len(hq_out) > 0:
+            results.append(hq_out.get())
+        results.sort()
+        # reassemble the chunks
+        output = []
+        for chunk_id, res in results:
+            output += res
+
+        end_time = datetime.datetime.now()
+        secs = end_time - start_time
+        print "Main took", secs
+
+        validation_sum = sum(output)
+        print "Total sum of elements (for validation):", validation_sum
+
+        if show_output:
+            if SHOW_IN_3D:
+                plotting.show_3D(output)
+            else:
+                plotting.show_2D(output)
+
+        return validation_sum
+
+    else:
+        # consume work and post partial results back into hq_out
+        while len(hq_in) == 0:
+            time.sleep(0.01)
+        while len(hq_in) > 0:
+            print "{} chunks of work to do".format(len(hq_in))
+
+            chunk_id, chunk = hq_in.get(block=True)
+            result = calculate_z(chunk)
+            hq_out.put((chunk_id, result))
+
+
+if __name__ == "__main__":
+    # get width, height and max iterations from cmd line
+    # 'python serial_python.py 1000 1000'
+    parser = argparse.ArgumentParser(description='Project description')
+    parser.add_argument('--size', '-s', type=int, help='Range for our width and height (e.g. 1000)', default=1000)
+    parser.add_argument('--maxiterations', '-m', type=int, help='Maximum number of iterations (e.g. 1000)', default=1000)
+    parser.add_argument('--plot3D', action="store_true", help='Plot in 3D (default is 2D)', default=False)
+    parser.add_argument('--server', action="store_true", help="", default=False)
+    args = parser.parse_args()
+    print args
+    w = h = args.size
+    maxiter = args.maxiterations
+    if args.plot3D:
+        SHOW_IN_3D = True
+    print "Using a width and height of {} and a maximum of {} iterations".format(w, maxiter)
+
+    # we can show_output for Python, not for PyPy
+    validation_sum = calc_pure_python(True, args)
+
+    if args.server:
+        # confirm validation output for our known test case
+        # we do this because we've seen some odd behaviour due to subtle student
+        # bugs
+        if w == 1000 and h == 1000 and maxiter == 1000:
+            assert validation_sum == 51214485  # if False then we have a bug
