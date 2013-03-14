05eb20b50bf15b477267c5165227ddb13ef43989
Ian Ozsvald <ian@ianozsvald.com>
2013-03-14 09:32:41 -0700

    added loglog cmd line option

--- 2_MapReduceDisco/check_word_frequencies.py
+++ 2_MapReduceDisco/check_word_frequencies.py
@@ -3,13 +3,18 @@ import json
 #import pylab
 import matplotlib.pyplot as plt
 
+# Usage
+# $ python check_word_frequencies.py --json_file=mapreduceout_wordcount_85917_1.json --loglog
+
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description='Project description')
     parser.add_argument('--json_file', help='JSON input file (e.g. mapreduceout_wordcount.json)', default="mapreduceout_wordcount.json")
+    parser.add_argument('--loglog', action="store_true", help='Draw loglog rather than linear', default=False)
     args = parser.parse_args()
 
     # read json lines, sort by frequency
+    print args
     lines = open(args.json_file).readlines()
     pairs = [json.loads(s) for s in lines]
     # sort by second item (frequency), reverse so most frequent comes first
@@ -18,7 +23,10 @@ if __name__ == "__main__":
     print pairs[:10]
 
     frequencies = [item[1] for item in pairs]
-    plt.loglog(frequencies)
+    if args.loglog:
+        plt.loglog(frequencies)
+    else:
+        plt.plot(frequencies)
     plt.ylabel("Log 10 Frequencies", fontsize=14, fontweight="bold")
     plt.xlabel("Rank", fontsize=14, fontweight="bold")
     plt.grid(True)
